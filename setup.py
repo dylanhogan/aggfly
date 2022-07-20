c869a89a903b16dd209c25e8d6e1032fb6f1fba4
Dylan Hogan <dth2133@columbia.edu>
2022-07-20 16:31:05 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     keywords="climate data, grids, aggregation",  # Optional
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={"": "aggfly"},  # Optional
+    # package_dir={"": "aggfly"},  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -112,7 +112,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where="aggfly"),  # Required
+    packages=find_packages(), #where="aggfly"),  # Required
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
