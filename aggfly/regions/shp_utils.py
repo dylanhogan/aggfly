5c7ef9b80b2f9f07a5602003e4f0f88da6e441c3
TomBearpark <t.bearpark@googlemail.com>
2022-10-14 14:22:21 -0500

    paths for shapefiles
    change from hardcoded to defaults

--- aggfly/regions/shp_utils.py
+++ aggfly/regions/shp_utils.py
@@ -6,8 +6,9 @@ import geopandas as gpd
 from functools import lru_cache
 
 @lru_cache(maxsize=None)
-def open_usa_shp(region_list=None):
-    usa = gpd.read_file("/home3/dth2133/repositories/aggfly/data/shapefiles/usa_simple_noHI.shp")
+def open_usa_shp(region_list=None, 
+        usa_shp_path = "/home3/dth2133/repositories/aggfly/data/shapefiles/usa_simple_noHI.shp"):
+    usa = gpd.read_file(usa_shp_path)
     usa['state'] = usa.HASC_1.str.slice(start=3)
     usa = usa.loc[usa.state!='AK']
     if region_list:
@@ -15,9 +16,9 @@ def open_usa_shp(region_list=None):
     return usa
 
 @lru_cache(maxsize=None)
-def open_counties_shp(region_list=None):
-    f = "/home3/dth2133/data/shapefiles/county/cb_2018_us_county_500k.shp"
-    counties = gpd.read_file(f)
+def open_counties_shp(region_list=None, 
+    usa_county_path =  "/home3/dth2133/data/shapefiles/county/cb_2018_us_county_500k.shp"):
+    counties = gpd.read_file(usa_county_path)
     counties['fips'] = counties.GEOID
     counties['STATEFP'] = counties.STATEFP.astype('int')
     counties = counties[(counties.STATEFP < 60) 
